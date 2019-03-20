fff820f29b65995d1437d197d131fc563f8a50ba
admin@example.com <jiangpeng@agilean.cn>
2019-03-20 10:22:11 +0800

    insect.py

--- insect.py
+++ insect.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python
 import re,os
+from time import strftime,gmtime
 
 
-print('---start to extract jsp files-----')
 
-'''file=open('/Users/zhangxiao/Desktop/python/samples','r')
-dest_file=open('/Users/zhangxiao/Desktop/python/lib_pom.xml','w')
-for line in file:
-  if (line.strip() !='' ):
-      m = re.match( r'(.*).jar', line, re.M|re.I)
-      print(m.group(1))
-      artifactId=m.group(1)
-      dest_file.write('<dependency>\t\n')
-      dest_file.write('<groupId>my</groupId>\t\n')
-      dest_file.write('<artifactId>'+artifactId+'</artifactId>\t\n')
-      dest_file.write('<version>0.0.0</version>\t\n')
-      dest_file.write('<scope>system</scope>\t\n')
-      dest_file.write('<systemPath>${base.dir}/src/main/webapp/WEB-INF/lib/'+line.strip()+'</systemPath>\t\n')
-      dest_file.write('</dependency>\t\n')
-
-file.close()
-'''
-
+time=strftime("%Y-%m-%d-%H%M%S", gmtime())
 
 path="/Users/zhangxiao/Desktop/python/samples"
 
-dest="/Users/zhangxiao/Desktop/python/samples/result.txt"
+dest="/Users/zhangxiao/Desktop/python/samples/result-"+time+".txt"
+
+print('----爬虫分析开始，源目标'+path+',分析结果呈现位置：'+dest)
 
 def deep_read(path):
 
-    dest_file=open(dest,'w')
+    dest_file=open(dest,'a')
     parents=os.listdir(path)
-
+    
     for parent in parents:
 
         child=os.path.join(path,parent)
@@ -39,43 +24,39 @@ def deep_read(path):
         if os.path.isdir(child):
            deep_read(child)
         else:
-           print('-file='+child)
-           file=open(child,'r',encoding='utf-8')
+           print('-处理文件：'+child)
+           file=open(child,'r',encoding='utf-8')    
+           extension=os.path.splitext(child)[1]
+           '''print('extension='+extension)'''
+           if ('.jsp' != extension):
 
-           row=0
-           
-           for line in file:
-               
-               
-               if ('' != line.strip()):
-                  row+=1
-                  print(row)
-                  repeats={}
-                  strs=re.findall(r'<emp:[a-zA-Z0-9]{1,}(.*)/>',line)
-                  
+               continue
+        
+           strs=re.findall(r'<emp:[a-zA-Z0-9]{1,}(.*)/?>',file.read().replace('\n','').replace('\t',''))          
+           for st in strs:
 
-                  for st in strs:
-
-                       '''print(st)'''
-                       array=re.findall(r'([a-zA-Z0-9]{1,}=[\"\']{1}[a-zA-Z0-9]{1,}[\"\']{1})',st)
-                       print(array)
-                       for kv in array:
-
-                           '''print(kv)'''
+               repeats={}
+               '''找出每一条自定义标签条目<emp:XXX> </emp>|<emp:XXX /> 中的key=value,统计重复的属性'''
+               array=re.findall(r'([a-zA-Z0-9]{1,}=[\"\']{1}[a-zA-Z0-9]{1,}[\"\']{1})',st)
+               '''print(array)'''
+               for kv in array:
                            
-                           if kv in repeats:
-                              repeats[kv]=repeats[kv]+1
-                           else:
-                              repeats[kv]=1
+                   if kv in repeats:
+                      repeats[kv]=repeats[kv]+1
+                   else:
+                      repeats[kv]=1
 
 
-                  for (k,v) in repeats.items():
-                        if (v>1):
-                           print('get the repeat one')
-                           print (k,v)
+               for (k,v) in repeats.items():
+                   if (v>1):
+                      '''print('get the repeat one')'''
+                      '''print (k,v)'''
 
-                           dest_file.write('file:'+child+',rows='+str(row)+',repeat key='+k+',times='+str(v)+'\t\n')
+                      dest_file.write('file:'+child+',repeat key:'+k+',times='+str(v)+'\t\n')
 
+           file.close()
+    dest_file.close()
+     
       
                  
                       
@@ -90,3 +71,5 @@ if __name__=='__main__':
    path="/Users/zhangxiao/Desktop/python/samples"
    
    deep_read(path)
+
+   print ('---本次爬虫分析结束----')
--- samples/A.jsp
+++ samples/A.jsp
@@ -3,4 +3,9 @@
 <emp:my abc="124" abc="124" xy="gt" xy="gt" />
 <emp:B x="a1" y="b1" z="b2" readonly="c" />
 
+<emp:B sample1="a1" sample1="a1" sample2="b2" sample2="b2" x="y"> </emp:B>
+
+<emp:C changerow1="a1" changerow1="a1" changerow23="23" changerow23="23"
+     fg="126">
+</emp>
 
